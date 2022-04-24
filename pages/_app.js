6c3d49391601836e492da81693ebc24b2ba5eb72
philjang <pjang7390@gmail.com>
2022-04-24 01:34:45 -0700

    font issue

--- pages/_app.js
+++ pages/_app.js
@@ -13,10 +13,10 @@ function MyApp({ Component, pageProps }) {
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous"></link>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Script src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" crossOrigin="anonymous"></Script>
       <Navbar />
       <Component {...pageProps} />
-        <Script src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" crossOrigin="anonymous"></Script>
-        <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossOrigin="anonymous"></Script>
+      <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossOrigin="anonymous"></Script>
     </>
 
   )
