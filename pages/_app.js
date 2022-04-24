dae6345c38a9f957267aed7fd34afcc18c87567b
philjang <pjang7390@gmail.com>
2022-04-24 01:29:59 -0700

    update portfolio

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Navbar from '../components/navbar'
 import '../styles/globals.css'
-import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -11,8 +10,8 @@ function MyApp({ Component, pageProps }) {
         <meta name="description" content="Phil Jang's Portfolio" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous"></link>
-        <Script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossOrigin="anonymous"></Script>
-        <Script src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" crossOrigin="anonymous"></Script>
+        <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossOrigin="anonymous"></script>
+        <script src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" crossOrigin="anonymous"></script>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
--- pages/contact.jsx
+++ pages/contact.jsx
@@ -3,7 +3,7 @@ export default function Contact() {
         <>
             <div className='d-flex flex-column center'>
                 <h1 className='my-5 animate__animated animate__fadeIn'>Let&apos;s get in touch.</h1>
-                <div className='contacts d-flex flex-column justify-content-around'>
+                <div className='contacts d-flex flex-column justify-content-around animate__animated animate__fadeIn'>
                     <a href={`mailto: philjang.pr@gmail.com`}><i className='fas fa-envelope'></i> philjang.pr@gmail.com </a>
                     <p className='mb-0'><i className="fas fa-mobile-alt"></i> +1-(425)-345-5813</p>
                     <a target="_blank" rel="noreferrer" href='https://www.linkedin.com/in/phil-jang'><i className='fab fa-linkedin'></i> phil-jang </a>
